3c30ddf4239b3b59c0a6563e41b4cfa61d4bfe14
abboodd <38141887+abboodd@users.noreply.github.com>
2018-05-26 13:42:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,21 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.on('ready', () => {
-    console.log('I am ready!');
+const Discord = require("discord.js");
+const LOka = new Discord.Client();
+console.log('By spark');
+LOka.on('ready', () => {
+  console.log(`Logged in as ${LOka.user.tag} !`);
+LOka.on('ready',  () => {   
+ 
 });
 
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+console.log('      ~            ~  By : spark ~           ~    ');
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+console.log(`Logged in as  * [ "  spark-YT " ] servers! [ " ${LOka.guilds.size} " ] Users! [ " ${LOka.users.size} " ]`);
+ 
+ 
+});
 client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
